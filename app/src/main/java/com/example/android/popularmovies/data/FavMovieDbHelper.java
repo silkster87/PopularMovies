60544cb25e70ef3fbf997292e9c948f903dd8ef9
Silky <silky.ng@gmail.com>
2018-02-02 08:06:56 +0000

    Completed FavMovieDbHelper class

--- app/src/main/java/com/example/android/popularmovies/data/FavMovieDbHelper.java
+++ app/src/main/java/com/example/android/popularmovies/data/FavMovieDbHelper.java
@@ -3,6 +3,7 @@ package com.example.android.popularmovies.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Silky on 01/02/2018.
@@ -19,14 +20,23 @@ public class FavMovieDbHelper extends SQLiteOpenHelper {
     public FavMovieDbHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-    
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_FAVMOVIE_TABLE = "CREATE TABLE " +
+                FavMovieContract.FavMovieEntry.TABLE_NAME + "( " +
+                FavMovieContract.FavMovieEntry._ID +"INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                FavMovieContract.FavMovieEntry.TITLE + "TEXT NOT NULL, " +
+                FavMovieContract.FavMovieEntry.MOVIE_IMAGE_PATH + "TEXT NOT NULL, " + " );";
 
+        sqLiteDatabase.execSQL(SQL_CREATE_FAVMOVIE_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        Log.w(LOG_TAG, "Upgrading database from version " + oldVersion + " to " + newVersion
+         + ". Old data will be destroyed");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + FavMovieContract.FavMovieEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
