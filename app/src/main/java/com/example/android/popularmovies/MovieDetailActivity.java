fb9faf06a3863b8b584542e024b1db535b313eee
Silky <silky.ng@gmail.com>
2018-02-03 15:55:04 +0000

    updated addFavMovie insert method

--- app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
@@ -1,14 +1,17 @@
 package com.example.android.popularmovies;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.popularmovies.data.FavMovieContract;
 import com.example.android.popularmovies.data.FavMovieDbHelper;
@@ -17,7 +20,6 @@ import com.squareup.picasso.Picasso;
 //When user clicks on a movie, this will set the view for more info on that movie.
 public class MovieDetailActivity extends AppCompatActivity {
 
-    private SQLiteDatabase db;
 
     private int vMovieID;
     private String vOriginalTitle;
@@ -34,15 +36,13 @@ public class MovieDetailActivity extends AppCompatActivity {
     private TextView mMovieRating;
 
     private MovieInfo mMovieInfo;
-    private FavMovieDbHelper mFavMovieDbHelper;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
         mMovieInfo = getIntent().getParcelableExtra("mMovieDetails");
-        mFavMovieDbHelper = new FavMovieDbHelper(this);
-        db = mFavMovieDbHelper.getWritableDatabase();
 
         mMovieThumbnail = (ImageView) findViewById(R.id.tv_movie_thumbnail);
         mMovieTitle = (TextView) findViewById(R.id.tv_movie_title);
@@ -81,7 +81,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         }
     }
 
-    private long addFavMovie(int movieID, String originalTitle, String releaseDate, String imageThumbPath,
+    private void addFavMovie(int movieID, String originalTitle, String releaseDate, String imageThumbPath,
                              String imagePath, String plotSynopsis, double userRating){
 
         ContentValues cv = new ContentValues();
@@ -94,7 +94,13 @@ public class MovieDetailActivity extends AppCompatActivity {
         cv.put(FavMovieContract.FavMovieEntry.PLOT_SYNOPSIS, plotSynopsis);
         cv.put(FavMovieContract.FavMovieEntry.USER_RATING, userRating);
 
-        return db.insert(FavMovieContract.FavMovieEntry.TABLE_NAME, null, cv);
+        ContentResolver resolver = getContentResolver();
+
+        Uri insertedUri = resolver.insert(FavMovieContract.FavMovieEntry.CONTENT_URI, cv);
+
+        if(insertedUri != null){
+            Toast.makeText(getBaseContext(), insertedUri.toString(), Toast.LENGTH_LONG).show();
+        }
 
     }
 }
