824bb47e9c3473dd978d0f42611c48f02c3adbd0
Silky <silky.ng@gmail.com>
2018-02-10 17:00:18 +0000

    changes to layout using justify text

--- app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +28,8 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 
 import java.net.URL;
+
+import me.biubiubiu.justifytext.library.JustifyTextView;
 
 //When user clicks on a movie, this will set the view for more info on that movie.
 public class MovieDetailActivity extends AppCompatActivity {
